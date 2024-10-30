4dae78880d25d58973ae646e220813b7c5385695
Awaish Lakhani <awaishkumar12@gmail.com>
2024-10-30 22:12:15 +0500

    Update Lab-01 (ATM).py.py

--- Lab-01 (ATM).py.py
+++ Lab-01 (ATM).py.py
@@ -1,13 +1,8 @@
-# Define correct username and password
 correct_username = "Awaish"
-correct_password = 1234
-
-# Ask user to enter username and password
+   correct_password = 1234
 username = input("Enter username: ")
-password = int(input("Enter password: "))
-
-# Check if both username and password are correct
-if username == correct_username and password == correct_password:
+   password = int(input("Enter password: "))
+         if username == correct_username and password == correct_password:
     print("Login successful")
 elif username != correct_username:
     print("Incorrect username")
